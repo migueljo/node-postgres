d7de9a0523c2f24afd74019856f77d239de81002
Miguel Martínez <migueljomartinez@gmail.com>
2022-02-28 22:24:41 -0500

    Applying dbUrl

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,13 +3,12 @@ const { Sequelize } = require('sequelize');
 const {config} = require('../config/config');
 const setupModels = require('../db/models')
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
-const sequelize = new Sequelize(URL, {
+const sequelize = new Sequelize(config.dbUrl, {
   dialect: 'postgres',
-  logging: true
+  logging: !config.isProd,
+  ssh: config.isProd ? {
+    rejectUnauthorized: false,
+  } : null
 });
 
 setupModels(sequelize);
