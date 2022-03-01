8e859c9501ecaab95f6c76ad6025df3af79ebb73
Miguel Martínez <migueljomartinez@gmail.com>
2022-02-28 23:06:54 -0500

    Migration fix

--- db/migrations/20220228022853-create-order.js
+++ db/migrations/20220228022853-create-order.js
@@ -4,6 +4,9 @@ const { ORDER_TABLE, OrderSchema } = require("../models/order.model");
 
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const OrderSchemaClone = {...OrderSchema};
+    delete OrderSchemaClone.total;
+
     await queryInterface.createTable(ORDER_TABLE, OrderSchema)
   },
 
